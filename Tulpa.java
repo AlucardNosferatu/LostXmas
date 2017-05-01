bc57004b390cbd1e0880bdb56c4d1fb3efd943e3
Ebenezer Scrooge <1641367382@qq.com>
2017-05-01 21:17:33 +0800

    Create Tulpa.java

---
+++ Tulpa.java
@@ -0,0 +1,44 @@
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
+public class Tulpa{
+  public String Name;
+  public int Age;
+  public int Health;
+  public int Hunger;
+  public int Mood;
+  public boolean Lived;
+  public boolean Gender;//true for female, false for male
+  
+  public Tulpa(String I_Name, Boolean I_Gender){
+    this.Lived=true;
+    this.Name=I_Name;
+    this.Gender=I_Gender;
+    this.Age=0;
+    this.Health=100;
+    this.Mood=100;
+  }
+  
+  public void live(Tulpa t){
+    if(t.Health<=0){
+      t.Lived=false;
+    }
+    else{
+      t.Lived=true;
+      t.Age++;
+    }
+  }
+  
+  public void eat(Tulpa t,Food f){
+    if(Lived){
+    }
+    else{
+      if(t.Gender){
+        System.out.println("Your Tulpa is dead, you should take good care of her!");
+      }
+      else{
+        System.out.println("Your Tulpa is dead, you should take good care of him!");
+      }
+    }
+  }
+}
