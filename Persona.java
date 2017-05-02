ec725bfb4c3d63f462688deddc8e8803da284ccc
Ebenezer Scrooge <1641367382@qq.com>
2017-05-02 11:41:55 +0800

    Update Persona.java

--- Persona.java
+++ Persona.java
@@ -3,9 +3,28 @@ import java.util.Random;
 public class Persona{
   
   public int growth;
-  public String tag[];
-  public Persona(){
+  public String tag[7];
+  public Persona(Tulpa t){
+    this.growth=0;
+    if(t.Health>=50){
+        if(t.Mood>=50){
+          this.tag[0]="Optimism";
+        }
+        else{
+          this.tag[0]="Realism";
+        }
+      }
+      else{
+        if(t.Mood>=50){
+          this.tag[0]="Romantism"
+        }
+        else{
+          this.tag[0]="Pessimism"
+        }
+      }
     
+      
+    Random ra=new Random();
   
   
   }
@@ -21,7 +40,7 @@ public class Persona{
   
   
   public boolean tag_check(String Input_Tag){
-    for(int i=1;i<this.tag.length;i++){
+    for(int i=0;i<this.tag.length;i++){
       if(this.tag[i].equals(Input_Tag)){
         return true;
       }
