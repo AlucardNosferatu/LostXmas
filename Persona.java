a29525c8e19e799f4be2a17f81cfb15050697fd1
Ebenezer Scrooge <1641367382@qq.com>
2017-05-02 11:24:24 +0800

    Update Persona.java

--- Persona.java
+++ Persona.java
@@ -1,7 +1,22 @@
 public class Persona{
   public int growth;
+  public String tag[];
+  public void action(){
   
-
-
-
+  
+  
+  }
+  
+  
+  public boolean tag_check(String Input_Tag){
+    for(int i=1;i<this.tag.length;i++){
+      if(this.tag[i]==Input_Tag){
+        return true;
+      }
+    }
+    return false;
+  }
+  
+  
+  
 }
