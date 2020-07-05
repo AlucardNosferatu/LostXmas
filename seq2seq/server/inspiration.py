80d2cd8dc9f023f3c643e538848e0acbb7179634
Ebenezer Scrooge <1641367382@qq.com>
2020-07-05 11:51:21 +0800

    Update inspiration.py

--- seq2seq/server/inspiration.py
+++ seq2seq/server/inspiration.py
@@ -1,11 +1,14 @@
-from cloudmusic.api import Api
-from advanced_quotes import topics, misc, quotes
+import time
+import random
 import bdtrans
 import cloudmusic
-import random
-import time
-
+from ltp import LTP
+from cloudmusic.api import Api
+from advanced_quotes import topics, misc
 from data.data_tool import remove_brackets
+from data.augmentation.frequency import getFreqDist
+
+pos = LTP()
 
 
 def search_lyrics(api_instance, para_dict):
@@ -16,8 +19,9 @@ def search_lyrics(api_instance, para_dict):
     return api_instance.send(url, param)
 
 
-def test_lyrics(keyword):
-    a = Api()
+def test_lyrics(keyword, a=None):
+    if a is None:
+        a = Api()
     para = {"string": keyword, "number": "100"}
     r = search_lyrics(a, para)
     lyrics_list = []
@@ -63,10 +67,82 @@ def test_quotes(keyword):
     return quote_list
 
 
+def gen_random_keywords():
+    with open('../infer/Online_A.txt', encoding='utf-8-sig', mode='r') as f:
+        lines = f.readlines()
+        words_with_dup = []
+        for line in lines:
+            words, hidden = pos.seg([line.replace('\n', '')])
+            words_pos = pos.pos(hidden)
+            words = words[0]
+            words_pos = words_pos[0]
+            for i in range(len(words)):
+                if len(words[i]) >= 2 and words_pos[i] in ['v', 'n']:
+                    words_with_dup.append(words[i])
+        freq_dist = getFreqDist(words_with_dup)
+    return freq_dist
+
+
+class Inspiration:
+    random_keywords = []
+    q_list = [
+        "在想啥呢？",
+        "有啥想法。。。？",
+        "想说些什么吗？",
+        "什么事？",
+        "怎么了？",
+        "不妨说说看？"
+    ]
+    a_list = []
+    q = ""
+    a = ""
+    keyword = ""
+    api = None
+
+    def __init__(self):
+        self.api = Api()
+        self.a_list = []
+        self.q_list = [
+            "在想啥呢？",
+            "有啥想法。。。？",
+            "想说些什么吗？",
+            "什么事？",
+            "怎么了？",
+            "不妨说说看？"
+        ]
+        self.a = ""
+        self.q = random.choice(self.q_list)
+        self.keyword = ""
+        self.random_keywords = gen_random_keywords()
+
+    def search_keyword(self, keyword=None):
+        self.q = random.choice(self.q_list)
+        if keyword is None:
+            keyword = self.keyword
+        assert len(keyword) > 0
+        quotes_list = test_quotes(keyword)
+        lyrics_list = test_lyrics(keyword, self.api)
+        self.a_list = quotes_list + lyrics_list
+
+    def select_reply(self, selected_index):
+        self.q = random.choice(self.q_list)
+        if len(self.a_list) > 0 and 0 <= selected_index < len(self.a_list):
+            self.a = self.a_list[selected_index]
+        else:
+            self.a = ""
+
+    def get_qa(self):
+        self.q = random.choice(self.q_list)
+        if len(self.a) == 0:
+            if len(self.a_list) == 0:
+                if len(self.random_keywords) == 0:
+                    self.random_keywords = gen_random_keywords()
+                self.search_keyword(random.choice(self.random_keywords))
+            self.a = random.choice(self.a_list)
+        return [self.q, self.a]
+
+
 if __name__ == '__main__':
     print('start')
-    searched = "开始"
-    ql = test_quotes(searched)
-    ll = test_lyrics(searched)
-    result = ql + ll
-    print(result)
+    Ins = Inspiration()
+    print(Ins.get_qa())
