12b26e42b2f7a38bb6b605d239f71174f2711170
Ebenezer Scrooge <1641367382@qq.com>
2023-04-03 10:58:25 +0800

    Update

--- ocr/ocr_encrypt.py
+++ ocr/ocr_encrypt.py
@@ -1,12 +1,24 @@
 import os
 
+from pyDes import des, CBC, PAD_PKCS5
+
 
 def encrypt_file(key, tgt_txt):
-    pass
+    with open(tgt_txt, 'rb') as f:
+        lines = f.read()
+    des_obj = des(key, CBC, key, pad=None, padmode=PAD_PKCS5)
+    lines_bytes = des_obj.encrypt(lines, padmode=PAD_PKCS5)
+    with open(tgt_txt.replace('.txt', '.xmas'), 'wb') as f:
+        f.write(lines_bytes)
 
 
 def decrypt_file(key, tgt_txt):
-    pass
+    with open(tgt_txt, 'rb') as f:
+        lines_bytes = f.read()
+    des_obj = des(key, CBC, key, pad=None, padmode=PAD_PKCS5)
+    lines = des_obj.decrypt(lines_bytes, padmode=PAD_PKCS5)
+    with open(tgt_txt.replace('.xmas', '.txt'), 'wb') as f:
+        f.write(lines)
 
 
 encrypt_key = input('input encryption key string:')
