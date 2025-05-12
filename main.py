d324b16fbd070de8174f0b573fda90cc2e91efa8
Ebenezer Scrooge <1641367382@qq.com>
2025-05-13 00:58:47 +0800

    Update

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from cfg import NEW_VOCAB, D_MODEL, N_HEADS, N_LAYERS
-from data import read_corpus, get_vocab
+import torch
+from torch import cosine_similarity
+
+from cfg import NEW_VOCAB, D_MODEL, N_HEADS, N_LAYERS, TRAIN_NEW
+from data import read_corpus, get_vocab, sentence_to_tensor
 from models.transformer_rag import TransformerRAG
 from tasks import train_rag_encode
 
@@ -11,5 +14,32 @@ if __name__ == '__main__':
         d_model=D_MODEL, nhead=N_HEADS, num_layers=N_LAYERS, vocab_size=len(words_list), max_length=max_length,
         pad_id=words_list.index('[PAD]')
     )
-    train_rag_encode(model=model, lines_words=lines_words, words_list=words_list, max_length=max_length)
+    if TRAIN_NEW:
+        train_rag_encode(model=model, lines_words=lines_words, words_list=words_list, max_length=max_length)
+    else:
+        model.load_state_dict(torch.load('weights/transformer_rag.pth'))
+    sentence_text_1 = '我很想你'
+    sentence_text_2 = '我想你'
+    sentence_text_3 = '你想吃什么'
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = model.to(device)
+    model.eval()
+    print(sentence_text_1, sentence_text_2, sentence_text_3)
+    sentence_tensor_1 = sentence_to_tensor(
+        sentence=sentence_text_1, words_list=words_list, max_length=max_length
+    ).to(device)
+    sentence_tensor_2 = sentence_to_tensor(
+        sentence=sentence_text_2, words_list=words_list, max_length=max_length
+    ).to(device)
+    sentence_tensor_3 = sentence_to_tensor(
+        sentence=sentence_text_3, words_list=words_list, max_length=max_length
+    ).to(device)
+    with torch.no_grad():
+        vec1, _ = model.vectorize_content(sentence_tensor_1)
+        vec2, _ = model.vectorize_content(sentence_tensor_2)
+        cs1 = cosine_similarity(x1=vec1, x2=vec2, dim=2)
+        cs1_s=(cs1*cs1).sum()
+        vec3, _ = model.vectorize_content(sentence_tensor_3)
+        cs2 = cosine_similarity(x1=vec1, x2=vec3, dim=2)
+        cs2_s=(cs2*cs2).sum()
     print('WIP')
